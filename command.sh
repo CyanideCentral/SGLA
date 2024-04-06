2497af20ac51a3e5169cfe24ddd06bbcea8e1473
gongyguo <444765201@qq.com>
2024-04-06 08:17:46 +0000

    fix requirement

--- command.sh
+++ command.sh
@@ -1,4 +1,4 @@
-OUTPUT=SMGF.log
+OUTPUT=SMGF_LA.log
 {
     python SMGF_LA.py --dataset acm --knn_k 10
     python SMGF_LA.py --dataset dblp --knn_k 10
@@ -8,6 +8,7 @@ OUTPUT=SMGF.log
     python SMGF_LA.py --dataset rm --knn_k 10
     python SMGF_LA.py --dataset amazon-photos --knn_k 10
     python SMGF_LA.py --dataset amazon-computers --knn_k 10
+    python SMGF_LA.py --dataset query --knn_k 20
     python SMGF_LA.py --dataset mageng --knn_k 10
     python SMGF_LA.py --dataset magphy --knn_k 10
 }|tee -a $OUTPUT
@@ -22,6 +23,7 @@ OUTPUT1=SMGF_PI.log
     python SMGF_PI.py --dataset rm --knn_k 10
     python SMGF_PI.py --dataset amazon-photos --knn_k 10
     python SMGF_PI.py --dataset amazon-computers --knn_k 10
+    python SMGF_PI.py --dataset query --knn_k 20
     python SMGF_PI.py --dataset mageng --knn_k 10
     python SMGF_PI.py --dataset magphy --knn_k 10
 }|tee -a $OUTPUT1
